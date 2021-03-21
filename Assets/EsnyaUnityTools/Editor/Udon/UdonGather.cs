277108779ce8d6b5a9c6bcfd0b83ca9b269d59ef
esnya <esnya@nekometer.com>
2021-03-21 14:39:11 +0900

    :sparkles: feat: Add Refine Udon

--- Assets/EsnyaUnityTools/Editor/Udon/UdonGather.cs
+++ Assets/EsnyaUnityTools/Editor/Udon/UdonGather.cs
@@ -1,30 +1,37 @@
 #if UDON && VRC_SDK_VRCSDK3
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.IO;
-using UnityEngine;
+using System.Linq;
 using UnityEditor;
+using UnityEngine;
 using VRC.Udon;
 
 namespace EsnyaFactory
 {
-    public class UdonGather {
-        private static IEnumerable<AbstractUdonProgramSource> EnumerateSelectedSerializedProgramAssets() {
+    [Serializable]
+    public class UdonGather
+    {
+        [SerializeField] private static string directory;
+        private static IEnumerable<AbstractUdonProgramSource> EnumerateSelectedSerializedProgramAssets()
+        {
             return Selection.objects
                 .Select(o => o as AbstractUdonProgramSource)
                 .Where(a => a?.SerializedProgramAsset != null && !string.IsNullOrEmpty(AssetDatabase.GetAssetPath(a?.SerializedProgramAsset)));
         }
 
-        [MenuItem("Assets/EsnyaTools/Gather Udon")][MenuItem("Assets/EsnyaTools/Gather Udon")]
-        private static void GatherUdon() {
+        [MenuItem("Assets/EsnyaTools/Gather Udon")]
+        [MenuItem("Assets/EsnyaTools/Gather Udon")]
+        private static void GatherUdon()
+        {
             var sources = EnumerateSelectedSerializedProgramAssets().ToList();
-            var directory = EditorUtility.SaveFolderPanel("Gather Udon Serialized Program Assets", "", "");
+            directory = EditorUtility.SaveFolderPanel("Gather Udon Serialized Program Assets", directory, "");
 
             if (string.IsNullOrEmpty(directory)) return;
 
             var targets = sources
-                .Select(source => {
+                .Select(source =>
+                {
                     var asset = source.SerializedProgramAsset;
                     var src = AssetDatabase.GetAssetPath(asset);
 
@@ -33,7 +40,8 @@ namespace EsnyaFactory
                     return (src, dst);
                 })
                 .Where(t => t.src != t.dst);
-            foreach (var (src, dst) in targets) {
+            foreach (var (src, dst) in targets)
+            {
                 MoveOrReplace(src, dst);
                 MoveOrReplace($"{src}.meta", $"{dst}.meta");
             };
@@ -42,17 +50,42 @@ namespace EsnyaFactory
         }
 
         [MenuItem("Assets/EsnyaTools/Gather Udon", true)]
-        private static bool GatharUdonValid() {
+        [MenuItem("Assets/EsnyaTools/Refine Udon", true)]
+        private static bool GatharUdonValid()
+        {
             return EnumerateSelectedSerializedProgramAssets().Any();
         }
 
-        private static void MoveOrReplace(string src, string dst) {
-            try {
+        private static void MoveOrReplace(string src, string dst)
+        {
+            try
+            {
                 Debug.Log($"Moving {src} to {dst}");
                 if (File.Exists(dst)) File.Delete(dst);
                 File.Move(src, dst);
-            } catch (Exception e) {
+            }
+            catch (Exception e)
+            {
                 Debug.LogError(e);
+            }
+        }
+
+        [MenuItem("Assets/EsnyaTools/Refine Udon")]
+        [MenuItem("Assets/EsnyaTools/Refine Udon")]
+        private static void RefineUdon()
+        {
+            directory = EditorUtility.OpenFolderPanel("Refine Udon Serialized Program Assetas", directory, "");
+            if (string.IsNullOrEmpty(directory)) return;
+
+            var sourcePaths = EnumerateSelectedSerializedProgramAssets().Select(source => AssetDatabase.GetAssetPath(source.SerializedProgramAsset)).ToList();
+            var paths = AssetDatabase.FindAssets("t:AbstractSerializedUdonProgramAsset", new[] { directory.Replace(Application.dataPath, "Assets") })
+                .Select(AssetDatabase.GUIDToAssetPath)
+                .Where(path => !sourcePaths.Where(sourcePath => sourcePath == path).Any());
+            foreach (var src in paths)
+            {
+                var dst = $"Assets/SerializedUdonPrograms/{Path.GetFileName(src)}";
+                MoveOrReplace(src, dst);
+                MoveOrReplace($"{src}.meta", $"{dst}.meta");
             }
         }
     }
