16ed23f58d775ca3fc25152e89f2cc5f633b39ed
esnya <esnya@nekometer.com>
2021-03-24 10:16:25 +0900

    :sparkles: feat: ffmpeg-normalize now replace original and creates backup

--- Assets/EsnyaUnityTools/Editor/FFMpegNormalize.cs
+++ Assets/EsnyaUnityTools/Editor/FFMpegNormalize.cs
@@ -1,45 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
-using UnityEngine;
 using UnityEditor;
+using UnityEngine;
 
-namespace EsnyaFactory.EsnyaUnityTools {
-    public class FFMpegNormalize {
+namespace EsnyaFactory.EsnyaUnityTools
+{
+    public class FFMpegNormalize
+    {
 
         [MenuItem("Assets/EsnyaTools/Normalize (ffmpeg-normalize)", false)]
-        private static void NormalizeSelected() {
-            foreach (var c in Selection.objects.Where(o => o.GetType() == typeof(AudioClip)).Select(o => o as AudioClip)) {
+        private static void NormalizeSelected()
+        {
+            foreach (var c in Selection.objects.Where(o => o.GetType() == typeof(AudioClip)).Select(o => o as AudioClip))
+            {
                 Normalize(c);
             }
         }
 
 
         [MenuItem("Assets/EsnyaTools/Normalize (ffmpeg-normalize)", true)]
-        private static bool IsNormalizeTarget() {
+        private static bool IsNormalizeTarget()
+        {
             return Selection.objects.Any(o => o.GetType() == typeof(AudioClip));
         }
 
-        private static void Normalize(AudioClip audioClip) {
-            var src = $"{new Regex("/Assets/?$").Replace(Application.dataPath, "")}/{AssetDatabase.GetAssetPath(audioClip)}";
-            var dst = $"{src}.normalized.wav";
-            var ext = "wav";
+        private static string GetOrCreateOriginal(string path)
+        {
+            var directory = Path.GetDirectoryName(path);
+            var filename = Path.GetFileNameWithoutExtension(path);
+            var extension = Path.GetExtension(path);
+
+            var original = Path.Combine(directory, $".{filename}.orig{extension}");
+
+            if (!File.Exists(original)) File.Copy(path, original);
+
+            return original;
+        }
+
+        private static void Normalize(AudioClip audioClip)
+        {
+            var path = $"{new Regex("/Assets/?$").Replace(Application.dataPath, "")}/{AssetDatabase.GetAssetPath(audioClip)}";
+            var dst = path;
+            var src = GetOrCreateOriginal(path);
+            var ext = Path.GetExtension(path);
             var nt = "peak";
             var target = 0;
 
-            try {
-                Exec("ffmpeg-normalize", $"\"{src}\" -ext \"{ext}\" -nt \"{nt}\" -t \"{target}\" -o \"{dst}\"");
-            } catch (System.Exception e) {
+            try
+            {
+                Exec("python3", $"-m ffmpeg_normalize -f \"{src}\" -ext \"{ext}\" -nt \"{nt}\" -t \"{target}\" -o \"{dst}\"");
+            }
+            catch (System.Exception e)
+            {
                 Debug.LogError(e);
-                if (EditorUtility.DisplayDialog("Error", "ffmpeg-normalize is not installed. Do you want to install now? (Python 3.x required)", "Install", "Cancel")) {
+                if (EditorUtility.DisplayDialog("Error", "ffmpeg-normalize is not installed. Do you want to install now? (Python 3.x required)", "Install", "Cancel"))
+                {
                     Exec("pip3", "install ffmpeg-normalize");
                     Normalize(audioClip);
                 }
             }
         }
 
-        private static void Exec(string command, string arguments) {
+        private static void Exec(string command, string arguments)
+        {
             Debug.Log($"{command} {arguments}");
             var process = System.Diagnostics.Process.Start(command, arguments);
             process.WaitForExit();
