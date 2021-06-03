1908c246d250a3dd6ff8cf44865053fc652a6de0
esnya <esnya@nekometer.com>
2021-06-03 17:25:27 +0900

    :lipstick: Update UI of CreditGenerator

--- Assets/EsnyaUnityTools/Scripts/CreditGenerator.cs
+++ Assets/EsnyaUnityTools/Scripts/CreditGenerator.cs
@@ -6,9 +6,9 @@ namespace EsnyaFactory {
   public class CreditGenerator : MonoBehaviour {
     public TMP_Text targetText;
     public string fileNames = "LICENSE OFL";
-    public string prefix = "Credits:";
-    public string suffix = "";
-    public string format = "  %name% [%license%]: %copyright%";
+    [TextArea] public string prefix = "Credits:";
+    [TextArea] public string suffix = "";
+    [TextArea] public string format = "  %name% [%license%]: %copyright%";
     [HideInInspector] public List<string> files;
   }
 }
