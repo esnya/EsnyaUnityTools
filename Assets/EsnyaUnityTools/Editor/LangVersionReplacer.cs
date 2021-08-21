b34ba661bc2177769ddaf40ce8d75a4ef87d417b
esnya <esnya@nekometer.com>
2021-08-21 21:44:40 +0900

    :sparkles: feat: LangVersionReplacer

---
+++ Assets/EsnyaUnityTools/Editor/LangVersionReplacer.cs
@@ -0,0 +1,35 @@
+#pragma warning disable IDE0051
+
+using UnityEditor;
+using UnityEngine;
+using System.IO;
+
+namespace EsnyaFactory
+{
+    public class LangVersionReplacer : AssetPostprocessor
+    {
+        [InitializeOnLoadMethod]
+        private static void ReplaceLangVersions()
+        {
+            var projectRoot = Path.GetDirectoryName(Application.dataPath);
+            Debug.Log(projectRoot);
+
+
+            foreach (var path in Directory.EnumerateFiles(projectRoot, "*.csproj"))
+            {
+                var content = File.ReadAllText(path);
+#if UNITY_2020_2_OR_NEWER
+                content = content.Replace("<LangVersion>latest</LangVersion>", "<LangVersion>8.0</LangVersion>");
+#elif UNITY_2019_2_OR_NEWER
+                content = content.Replace("<LangVersion>latest</LangVersion>", "<LangVersion>7.3</LangVersion>");
+#endif
+                File.WriteAllText(path, content);
+            }
+        }
+
+        private static void OnGeneratedCSProjectFiles()
+        {
+            ReplaceLangVersions();
+        }
+    }
+}
