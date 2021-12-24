b82218a4c110d758401371a375370f41532e0266
esnya <esnya@nekometer.com>
2021-12-24 17:28:52 +0900

    :sparkles: feat(rename-tool): Add name confrict scanner

--- Assets/EsnyaUnityTools/Editor/RenameTool.cs
+++ Assets/EsnyaUnityTools/Editor/RenameTool.cs
@@ -1,7 +1,11 @@
 using System.Collections.Generic;
 using System.Linq;
+using BestHTTP.SecureProtocol.Org.BouncyCastle.Math.EC;
+using Codice.Client.Common;
+using NUnit.Framework.Constraints;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace EsnyaFactory
 {
@@ -29,15 +33,61 @@ namespace EsnyaFactory
             serializedObject.Update();
             serializedObject.ApplyModifiedProperties();
 
-
             if (GUILayout.Button("Unique Name For Sibling"))
             {
-                Undo.RecordObjects(Selection.gameObjects, "Rename Tool");
-                foreach (var gameObject in Selection.gameObjects.OrderBy(o => o.transform.GetSiblingIndex()))
+                UniqueNameForSiblings(Selection.gameObjects);
+            }
+
+            if (GUILayout.Button("Scan Name Conflictions"))
+            {
+                DupScan();
+            }
+
+            if (dupes != null)
+            {
+                if (dupes.Length == 0)
                 {
-                    gameObject.name = UnityEditor.GameObjectUtility.GetUniqueNameForSibling(gameObject.transform.parent, gameObject.name);
+                    EditorGUILayout.HelpBox("No conflicts", MessageType.Info);
+                }
+                else
+                {
+                    using (new EditorGUILayout.HorizontalScope())
+                    {
+                        EditorGUILayout.HelpBox($"{dupes.Length} GameObject(s) has conflicted name", MessageType.Info);
+                        if (GUILayout.Button("Fix"))
+                        {
+                            UniqueNameForSiblings(dupes);
+                            dupes = null;
+                        }
+                    }
                 }
             }
+        }
+
+        private void UniqueNameForSiblings(GameObject[] targets)
+        {
+            Undo.RecordObjects(targets, "Rename Tool");
+            foreach (var (gameObject, index) in targets.OrderBy(o => o.transform.GetSiblingIndex()).Select((gameObject, index) => (gameObject, index)))
+            {
+                EditorUtility.DisplayProgressBar("Rename Tool", $"{gameObject.name}", (float)index / targets.Length);
+                gameObject.name = UnityEditor.GameObjectUtility.GetUniqueNameForSibling(gameObject.transform.parent, gameObject.name);
+            }
+            EditorUtility.ClearProgressBar();
+        }
+
+        private GameObject[] dupes;
+        private void DupScan()
+        {
+            dupes = SceneManager.GetActiveScene()
+                .GetRootGameObjects()
+                    .SelectMany(o => o.GetComponentsInChildren<Transform>())
+                    .Select(t => t.gameObject)
+                    .GroupBy(o => o.name)
+                    .Select(g => g.ToArray())
+                    .Where(a => a.Length > 2 && a.Select(o => o.transform.parent).Distinct().Count() < a.Length)
+                    .SelectMany(g => g)
+                    .Distinct()
+                    .ToArray();
         }
     }
 }
